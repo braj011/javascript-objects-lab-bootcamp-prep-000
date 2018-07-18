871e5f62c2664d16456ed4e7bdb13c9d6cca935e
braj011 <bhagyaraj01@gmail.com>
2018-07-18 15:28:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,3 +42,26 @@ describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', functio
     })
   })
 */
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+
+
+
+describe('deleteFromObjectByKey(object, key)', function() {
+    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+      var newObj = deleteFromObjectByKey(obj, 'prop')
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('does not modify the original object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+
+      deleteFromObjectByKey(obj, 'prop')
+      expect(obj['prop']).toBe(1)
+    })
+  })
