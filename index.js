44dad921d1987ab7daafa3c8fbbfc4c3ce4f8fc6
braj011 <bhagyaraj01@gmail.com>
2018-07-18 15:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 } 
 
 
-
-// KEY BIT!! it('returns an object with the orignal key value pairs and the new key value pair', function() {
-
 /*
-
-
   describe('updateObjectWithKeyAndValue(object, key, value)', function() {
     it('returns an object with the orignal key value pairs and the new key value pair', function() {
       var obj = { prop: 1 }
@@ -20,14 +15,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
         prop2: 2
       })
     })
+*/
 
-    it('it does not modify the original object, but rather returns a clone with the new data', function() {
-      var obj = { prop: 1 }
-
-      updateObjectWithKeyAndValue(obj, 'prop2', 2)
-
-      expect(obj['prop2']).toBe(undefined)
-    })
-  })
 
 */
