bef7ea25531960239dff4fc3e3b71f5464b3a8d0
braj011 <bhagyaraj01@gmail.com>
2018-07-18 14:56:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return recipes;
+  
+  
+  return Object.assign({}, object, { [key]: value })
+}
+
+// KEY BIT!! it('returns an object with the orignal key value pairs and the new key value pair', function() {
+
+/*
+
+
+  describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+    it('returns an object with the orignal key value pairs and the new key value pair', function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+
+    it('it does not modify the original object, but rather returns a clone with the new data', function() {
+      var obj = { prop: 1 }
+
+      updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+      expect(obj['prop2']).toBe(undefined)
+    })
+  })
+
+*/
